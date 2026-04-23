740ae330a8ad85798ff85ddc70e72c3aa23e84b2
sanoos7 <66918830+sanoos7@users.noreply.github.com>
2026-04-23 21:50:54 +0300

    Fix bot token in sendMessage API call

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def webhook():
     if "text" in message:
         if message["text"] == "/start":
             requests.post(
-                f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
+                f"https://api.telegram.org/bot{8557808707:AAE9AXJELXwjURuU3RM03C99QyC2qpOsXA4}/sendMessage",
                 data={
                     "chat_id": chat_id,
                     "text": "🎧 Send me an audio file to check if it's Lossless or Lossy"
